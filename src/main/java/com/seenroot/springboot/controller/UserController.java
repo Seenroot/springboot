214d100e7558cb6bc34be8591ca26eba000a0cfe
seenroot <1207470844@qq.com>
2019-07-03 09:04:37 +0800

    接口统一添加前缀/api

--- src/main/java/com/seenroot/springboot/controller/UserController.java
+++ src/main/java/com/seenroot/springboot/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.seenroot.springboot.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
     @PostMapping("/login/account")
